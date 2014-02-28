b83c645adede8b1da0f7e58ef15b4bdcfbdf827e
Max Glenister <moglenstar@gmail.com>
2014-02-28 10:29:52 +0000

    Removed some now useless files

--- addpage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [[ "${1}" == "" ]]; then
-	echo -e "No title specified!"
-else
-	echo -e "Creating new page: ${1}"
-	rake page name="${1}"
-fi
--- addpost.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [[ "${1}" == "" ]]; then
-	echo -e "No title specified!"
-else
-	echo -e "Creating new post: ${1}"
-	rake post title="${1}"
-fi
