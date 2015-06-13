4b3316714d97c5cc261a4cadc4140b5be5135321
Max Glenister <moglenstar@gmail.com>
2015-06-13 16:51:09 +0100

    Made 'recent' lists in sidebar render on client-side

---
+++ assets/main.js
@@ -0,0 +1,42 @@
+(function (window, $) {
+    'use strict';
+    var sections = ['.featured-posts', '.recent-posts', '.recent-reviews'];
+
+    var console = window.console;
+
+    var tpl = function(template, data) {
+        for (var token in data) {
+            var value = data[token];
+            template = template.replace(new RegExp("{" + token + "}", "g"), value);
+        }
+        return template;
+    };
+
+    var handle_data = function(data) {
+        $.each(sections, function (i, v) {
+            var $this = $(v);
+            var key = v.split('.')[1];
+            var dataset = data[key];
+
+            if ($this.length) {
+                var html = '';
+                $.each(dataset, function () {
+                    html += tpl('<li><a href="{link}"><span class="title">{title}</span><span class="date">{published}</span></a></li>', this);
+                });
+
+                $this.html(html);
+            }
+        });
+
+    };
+
+    var fetch_feed = function () {
+        var url = '/posts.json';
+
+        $.ajax(url).done(handle_data);
+    };
+
+    $(function () {
+        fetch_feed();
+    });
+}(window, window.jQuery));
