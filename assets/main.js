a473122af30109fc3e60b0304e1f9b7e10d82543
Max Glenister <moglenstar@gmail.com>
2015-08-19 17:03:24 +0100

    Design overhaul (Again....)

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-(function (window, $) {
+(function(window, $) {
     'use strict';
     var sections = ['.featured-posts', '.recent-posts', '.recent-reviews'];
 
@@ -13,15 +13,15 @@
     };
 
     var handle_data = function(data) {
-        $.each(sections, function (i, v) {
+        $.each(sections, function(i, v) {
             var $this = $(v);
             var key = v.split('.')[1];
             var dataset = data[key];
 
             if ($this.length) {
                 var html = '';
-                $.each(dataset, function () {
-                    html += tpl('<li><a href="{link}"><span class="title">{title}</span><span class="date">{published}</span></a></li>', this);
+                $.each(dataset, function() {
+                    html += tpl('<li><a href="{link}" title="Published on {published}">{title}</a></li>', this);
                 });
 
                 $this.html(html);
@@ -30,13 +30,13 @@
 
     };
 
-    var fetch_feed = function () {
+    var fetch_feed = function() {
         var url = '/posts.json';
 
         $.ajax(url).done(handle_data);
     };
 
-    $(function () {
+    $(function() {
         fetch_feed();
     });
 }(window, window.jQuery));
