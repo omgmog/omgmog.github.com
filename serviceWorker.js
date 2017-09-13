15d8a82485860eb106cafb79e50c6479b1eadaf6
Max Glenister <moglenstar@gmail.com>
2017-09-13 12:48:38 +0100

    Added additional assets to serviceWorker

--- serviceWorker.js
+++ serviceWorker.js
@@ -5,49 +5,54 @@ layout: # no layout
 'use strict';
 
 const cache = {
-    version: '{% if site.github %}{{site.github.build_revision}}{% endif %}'
+  version: '{% if site.github %}{{site.github.build_revision}}{% endif %}'
 };
 
 self.addEventListener('fetch', event => {
-    const url = new URL(event.request.url);
+  const url = new URL(event.request.url);
 
-    if (url.pathname.startsWith('/post/')) {
-        caches.open(cache.version)
-            .then(cache => cache.add(url));
-    }
+  if (url.pathname.startsWith('/post/')) {
+    caches.open(cache.version)
+      .then(cache => cache.add(url));
+  }
 
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-            .catch(() => {
-                if (event.request.mode === 'navigate') {
-                    return caches.match('/offline.html');
-                }
-            })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('/offline.html');
+        }
+      })
+  );
 });
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(cache.version)
-            .then(cache => cache.addAll([
-                '/assets/pygments.css',
-                '/assets/style.css',
-                '/offline.html'
-            ]))
-    );
+  event.waitUntil(
+    caches.open(cache.version)
+      .then(cache => cache.addAll([
+        '/assets/pygments.css',
+        '/assets/style.css',
+        '/assets/star.svg',
+        '/assets/star-filled.svg',
+        '/assets/comments.css',
+        '/assets/marked.js',
+        '/assets/comments.js',
+        '/offline.html'
+      ]))
+  );
 });
 
 self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cache.version !== cacheName) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cache.version !== cacheName) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
