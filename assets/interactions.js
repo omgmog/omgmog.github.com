87a2db284017871909a981788340b702ce4ab083
Max Glenister <moglenstar@gmail.com>
2022-03-29 13:20:16 +0100

    Tidy up interactions js even more, grab new data after 1 hour, generate unique fallback pixel avatars when they fail to load

--- assets/interactions.js
+++ assets/interactions.js
@@ -4,10 +4,6 @@
 
     const GH_FETCH_OPTS = {Accept: "application/vnd.github.v3.full+json", cache: "force-cache"};
   
-
-    const module = {};
-
-    // mentiontypes
     const TYPES = {
         'comment': {
             template: document.getElementById('tpl-reply').innerHTML,
@@ -75,47 +71,47 @@
         }
     };
 
-    module.fetchGithubComments = async function (issueID) {
-        if (!issueID) return;
-        this.issue = await fetch(`https://api.github.com/repos/omgmog/omgmog.github.com/issues/${issueID}`, GH_FETCH_OPTS).then(response => response.json());
-        this.comments = await fetch(this.issue.comments_url, GH_FETCH_OPTS).then(response => response.json());
-        return this.comments;
-    };
-    module.fetchWebmentions = async function (url) {
-        this.webmentions = await fetch(`https://webmention.io/api/mentions.jf2?target=${url}`).then(response => response.json());
-        return this.webmentions;
-    };
+    const pageURL_base64 = btoa(pageURL);
 
-    module.standardOwner = (who) => {
-        // Just to normalize my own presence in the comments and webmentions
-        if (
-            (
-                who.login === 'omgmog' ||
-                who.name === 'Max Glenister'
-            ) && (
-                who.html_url === 'https://github.com/omgmog' ||
-                who.url === 'https://twitter.com/omgmog'
-            )
-        ) {
-            return {
-                login: 'Max Glenister (omgmog)',
-                name: 'Max Glenister (omgmog)',
-                html_url: who.html_url,
-                url: who.url,
-                avatar_url: '/assets/max2020-1a.png',
-                photo: '/assets/max2020-1a.png',
+    const module = {};
+
+    module.fetchGithubIssue = async issueID => await fetch(`https://api.github.com/repos/omgmog/omgmog.github.com/issues/${issueID}`, GH_FETCH_OPTS).then(response => response.json());
+
+    module.fetchGithubComments = async issueData => await fetch(issueData.comments_url, GH_FETCH_OPTS).then(response => response.json());
+
+    module.fetchWebmentions = async url => await fetch(`https://webmention.io/api/mentions.jf2?target=${url}`).then(response => response.json());
+
+    module.renderThing = (type, data) => {
+        let template = type.template;
+        const attributes = Object.keys(type.attributes);
+        for (const index in attributes) {
+            const attribute = attributes[index];
+            let value = data[type.attributes[attribute]];
+            if (!value) {
+                value = type.attributes[attribute];
             }
-        } else {
-            return who;
-        }
-    };
+            if (!value || (typeof value !== 'string')) {
+                value = data[type.attributes[attribute][0]];
+            }
+            if (!value || (typeof value !== 'string')) {
+                value = data[type.attributes[attribute][0]][type.attributes[attribute][1]];
+            }
 
-    module.saveData = (what, where) => {
-        localStorage.setItem(where, JSON.stringify(what));
-    }
-    module.loadData = (what) => {
-        return JSON.parse(localStorage.getItem(what));
-    }
+            if (attribute === 'date') {
+                value = new Intl.DateTimeFormat('default', {
+                    year: 'numeric', 
+                    month: 'short', 
+                    day: 'numeric'
+                }).format(new Date(value));
+            }
+            if (attribute === 'body') {
+                value = marked.parse(value);
+            }
+
+            template = template.replace(new RegExp('%' + attribute + '%', 'g'), value);
+        }
+      return template;
+    };
 
     module.renderThings = things => {
         let type;
@@ -132,47 +128,51 @@
             element.style.display = 'block';
             element.querySelector('.items').innerHTML += module.renderThing(type, item);
         });
-    }
-    module.renderThing = (type, data) => {
-        let template = type.template;
-        const attributes = Object.keys(type.attributes);
-        for (const index in attributes) {
-            const attribute = attributes[index];
-            let value = data[type.attributes[attribute]];
-            if (!value) {
-                // probably not on the original object, but we're adding it in the type..
-                value = type.attributes[attribute];
-            }
-            if (!value || (typeof value !== 'string')) {
-                value = data[type.attributes[attribute][0]];
-            }
-            if (!value || (typeof value !== 'string')) {
-                value = data[type.attributes[attribute][0]][type.attributes[attribute][1]];
-            }
+    };
 
-            // special cases to handle dates and bodies
-            if (attribute === 'date') {
-                value = new Intl.DateTimeFormat('default', {
-                    year: 'numeric', 
-                    month: 'short', 
-                    day: 'numeric'
-                }).format(new Date(value));
+    module.makeFallbackAvatar = (text_value, img_element) => {
+        const p = 2;
+        const canvas = document.createElement('canvas');
+        canvas.style.imageRendering='pixelated';
+        canvas.width = 14;
+        canvas.height = 14;
+        const context = canvas.getContext('2d');
+        const r = 1;
+
+        if (text_value) {
+            for (
+                let r = 1, i = 28 + text_value.length;
+                i--;
+            ) {
+                // xorshift32
+                (r ^= r << 13), (r ^= r >>> 17), (r ^= r << 5);
+                const X = i & 3,
+                Y = i >> 2;
+                if (i >= 28) {
+                    // seed state
+                    r += text_value.charCodeAt(i - 28);
+                    context.fillStyle = '#' + ((r >> 8) & 0xffffff).toString(16).padStart(0, 6);
+                } else {
+                    // draw pixel
+                    if (r >>> 29 > (X * X) / 3 + Y / 2) {
+                        context.fillRect(p * 3 + p * X, p * Y, p, p);
+                        context.fillRect(p * 3 - p * X, p * Y, p, p);
+                    }
+                }
             }
-            if (attribute === 'body') {
-                value = marked.parse(value);
-            }
-            template = template.replace(new RegExp('%' + attribute + '%', 'g'), value);
+            img_element.src = canvas.toDataURL();
         }
-      return template;
     };
 
     module.checkForFailedAvatars = () => {
         document.querySelectorAll('#interactions .avatar').forEach(avatar => {
-            avatar.onerror = err => avatar.classList.add('load-failed');
+            avatar.onerror = err => module.makeFallbackAvatar(avatar.dataset.username, avatar);
         });
-    }
+    };
 
-    const pageURL_base64 = btoa(pageURL);
+    module.saveData = (what, where) => localStorage.setItem(where, JSON.stringify(what));
+
+    module.loadData = what => JSON.parse(localStorage.getItem(what));
 
     let interactions = {
         date: module.loadData(`interactions-date-${pageURL_base64}`) || 0,
@@ -182,13 +182,12 @@
 
     // is it more than a day old?
     const now = Date.now();
-    const oneDay = 24 * 60 * 60 * 1000;
-    const olderThanOneDay = new Date(interactions.date) < (now - oneDay);
-    if (olderThanOneDay) {
+    const oneHour = 60 * 60 * 1000;
+    if (new Date(interactions.date) < (now - oneHour)) {
         module.saveData(now, `interactions-date-${pageURL_base64}`);
 
         if (githubIssueID) {
-            module.fetchGithubComments(githubIssueID).then(comments => {
+            module.fetchGithubIssue(githubIssueID).then(issue => module.fetchGithubComments(issue).then(comments => {
                 if (!comments) return;
                 interactions.comments = {
                     type: 'comment',
@@ -197,7 +196,7 @@
                 module.saveData(interactions.comments, `interactions-comments-${pageURL_base64}`);
                 module.renderThings(interactions.comments);
                 module.checkForFailedAvatars();
-            });
+            }));
         }
         if (pageURL) {
             module.fetchWebmentions(pageURL).then(mentions => {
