de49d796f09728eaad4501056387e169b363a629
Max Glenister <moglenstar@gmail.com>
2026-02-13 15:16:19 +0000

    Get blog ready for old content import

--- _plugins/archived_links.rb
+++ _plugins/archived_links.rb
@@ -4,52 +4,50 @@ Jekyll::Hooks.register :posts, :post_render do |post|
   next unless post.data['archived'] == true
 
   site_url = post.site.config['url'] || ''
-  site_baseurl = post.site.config['baseurl'] || ''
   site_domain = site_url.gsub(/https?:\/\//, '')
 
   # Get the year from the post date
   post_year = post.date.year
 
-  # Only process links within the main article content
-  # Split at </article> to avoid processing templates and interactions
-  if post.output =~ /(<article class="post h-entry">.*?<\/article>)/m
+  # Rewrite external links to point to archive.org
+  rewrite_links = lambda do |html|
+    html.gsub(/<a\s+([^>]*?)href=["']([^"']+)["']([^>]*?)>/i) do
+      before_href = $1
+      url = $2
+      after_href = $3
+
+      skip_link = url.start_with?('/') ||
+                  url.start_with?('#') ||
+                  url.include?('archive.org') ||
+                  url.include?(site_domain) ||
+                  url.start_with?('mailto:') ||
+                  url.start_with?('javascript:')
+
+      if skip_link
+        "<a #{before_href}href=\"#{url}\"#{after_href}>"
+      else
+        archived_url = "https://web.archive.org/web/#{post_year}*/#{url}"
+        "<a #{before_href}href=\"#{archived_url}\"#{after_href}>"
+      end
+    end
+  end
+
+  # Process links within the main article content
+  if post.output =~ /(<article class="post h-entry[\w\-\s]*?">.*?<\/article>)/m
     before_article = $`
-    article_content = $1
+    matched_article = $1
     after_article = $'
+    post.output = before_article + rewrite_links.call(matched_article) + after_article
+  end
 
-    # Parse and modify links only in the article content
-    article_content = article_content.gsub(/<a\s+([^>]*?)href=["']([^"']+)["']([^>]*?)>/i) do
-    before_href = $1
-    url = $2
-    after_href = $3
-
-    # Skip if:
-    # - Internal link (starts with / or #)
-    # - Already an archive.org link
-    # - Link to same domain
-    # - Mailto link
-    # - JavaScript link
-    skip_link = url.start_with?('/') ||
-                url.start_with?('#') ||
-                url.include?('archive.org') ||
-                url.include?(site_domain) ||
-                url.start_with?('mailto:') ||
-                url.start_with?('javascript:')
-
-    if skip_link
-      # Return original link unchanged
-      "<a #{before_href}href=\"#{url}\"#{after_href}>"
-    else
-      # Wrap with archive.org using post year
-      archived_url = "https://web.archive.org/web/#{post_year}*/#{url}"
-      "<a #{before_href}href=\"#{archived_url}\"#{after_href}>"
+  # Process links within the archived comments section
+  # Boundary: from <div id="archived-comments"> to the next <h3 (which starts Comments/Webmentions)
+  if post.output.include?('id="archived-comments"')
+    post.output = post.output.sub(/(<div id="archived-comments">)(.*?)(<h3 )/m) do
+      opener = $1
+      comments_html = $2
+      closer = $3
+      opener + rewrite_links.call(comments_html) + closer
     end
-    end
-
-    # Reconstruct the output with templates/interactions unchanged
-    post.output = before_article + article_content + after_article
-  else
-    # If article tag not found, don't process anything
-    # (this shouldn't happen in normal posts)
   end
 end
